b9c11a0a27e2a0d2d4bb109119ee6be12ed22998
zeynepesila <gurelzeynepesila@gmail.com>
2025-08-04 08:44:04 +0300

    04.08.2025 part 1

--- blog-frontend/src/main.js
+++ blog-frontend/src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import axios from 'axios'
 import axios from 'axios';
 
 // Axios için temel URL'yi ayarla
